20e4c344f6e3fc2133a70aa3e94331351afd73b5
Aleksander Wójtowicz <anuar2k@outlook.com>
2018-12-09 15:40:08 +0100

    SAD-35: Added support for non-standard openmrs instance paths

--- app/js/route/openMRSRoute.js
+++ app/js/route/openMRSRoute.js
@@ -8,16 +8,8 @@ OWARoutes
              */
             getOpenmrsUrl: function () {
                 var location = window.location.toString();
-                var serverLocation = location.substring(0, location.indexOf('/owa/')); // http://localhost:8080/openmrs/server1
-                var last_string = serverLocation.substring(serverLocation.lastIndexOf('/') + 1, serverLocation.length).toLowerCase(); // server1
-
-                var finallocation = serverLocation;
-                var isOpenMRS = last_string.indexOf('openmrs');
-
-                if (isOpenMRS < 0) {
-                    finallocation = serverLocation.substring(0, serverLocation.lastIndexOf('/')); // http://localhost:8080/openmrs
-                }
-                return finallocation;
+                var serverLocation = location.substring(0, location.indexOf('/owa/')); // http://localhost:8080/openmrs
+                return serverLocation;
             }
         }
     }]);
